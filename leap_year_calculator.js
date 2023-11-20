223cc203636570e9a0ffc2c68fa0f52e4bc7f8e5
isoldaliborio <isoldaliborio@gmail.com>
2023-11-20 17:32:24 +0000

    teacher solution

--- leap_year_calculator.js
+++ leap_year_calculator.js
@@ -30,3 +30,23 @@ function isLeap(year) {
     }
 
 console.log(isLeap(1987));
+
+
+function isLeapYear(year) {
+    if (year % 4 === 0){
+        if (year % 100 === 0) {
+            if (year % 400 === 0)
+                return "Leap year"
+            else {
+                return "Not leap year."
+            }
+        }
+        else {
+            return " Leap year"
+        }
+    }
+    else {
+        return "Not leap year."
+    }
+    
+}
