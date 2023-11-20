b10024503970abaccf500811918418b29aaaaa94
isoldaliborio <isoldaliborio@gmail.com>
2023-11-20 14:32:27 +0000

    first commit

---
+++ love_calculator.js
@@ -0,0 +1,11 @@
+var a = prompt("What is your name?");
+var b = prompt("what is theier name?");
+var loveScore = Math.random();
+loveScore = Math.floor((loveScore * 100) + 1)
+
+
+if (loveScore > 70) {
+    alert(" you match " + loveScore + "%. You love each other like Kanye loves Kanye");
+} else {
+    alert(" you match " + loveScore + "%. ");
+}
