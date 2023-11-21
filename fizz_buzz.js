35e87bb5176821d91b8dff31561f083b76e730e5
isoldaliborio <isoldaliborio@gmail.com>
2023-11-21 12:38:33 +0000

    fizzBuzz challenge

---
+++ fizz_buzz.js
@@ -0,0 +1,26 @@
+/* Write a program that prints the numbers from 1 to 100. 
+But for multiples of three print "FIzz" intead of the number and 
+for the multiples of five print "Buzz". 
+For numbers which are multiples of both three and five print "FizzBuzz". */
+
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    }
+    else if (count % 3 === 0) {
+        output.push("Fizz");
+    }
+     else if (count % 5 === 0) {
+         output.push("Buzz");
+    }
+    else {
+        output.push(count);
+    }
+    count++; 
+        console.log(output);
+}
+fizzBuzz();
