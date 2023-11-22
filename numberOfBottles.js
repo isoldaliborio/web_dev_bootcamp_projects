1132d5a62421e91b992f56b44a60781b992e31aa
isoldaliborio <isoldaliborio@gmail.com>
2023-11-22 19:30:17 +0000

    number of bottles challenge

--- numberOfBottles.js
+++ numberOfBottles.js
@@ -6,23 +6,23 @@ function numberOfBottles() {
         if (numberOfBottles === 1) {
             bottleWord = "bottle";
         } 
-        if (numberOfBottles === 0) {
-            numberOfBottles = "No more";
-            bottleWord = "bottles";
-        }
+        // if (numberOfBottles === 0) {
+        //     numberOfBottles = "No more";
+        //     bottleWord = "bottles";
+        // }
         var message1 = numberOfBottles + " " + bottleWord + " of beer on the wall, " 
         + numberOfBottles + " "  + bottleWord + " of beer."; 
         numberOfBottles--;
 
         if (numberOfBottles === 0) {
             numberOfBottles = "No more";
-            bottleWord = "bottles";
+            bottleWord = "bottles";    
         }
         var message2 = "Take one down and pass it arround, "  + numberOfBottles + " "  + bottleWord + " of beer on the wall.";
-        numberOfBottles--;
         console.log(message1 + message2);
-        } 
 
+        }
+        console.log("No more bottles on the wall, no more bottles of beer. Got to the story and buy more, 99 bottles od beer on the wall.");
     }
 
 numberOfBottles();
